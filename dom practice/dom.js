b95e4cc8ba84c16fa941fc20f58005e87441e49d
Umair Usman <163662676+Sexa00@users.noreply.github.com>
2024-03-23 01:00:37 +0500

    Add files via upload

---
+++ dom practice/dom.js
@@ -0,0 +1,12 @@
+// function update() { 
+//     console.log("hello")
+    
+// }
+function update() { 
+    console.log("updating");
+
+
+var input = document.getElementById('myinput');
+var output = document.getElementById('output');
+output.innerHTML= input.value;
+}
